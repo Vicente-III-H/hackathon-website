36870af2711c6e57d2e4e40faa4c889a8f9d4b0c
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2025-10-08 11:58:24 -0400

    Add basic hero section elements

---
+++ src/hero/hero.jsx
@@ -0,0 +1,22 @@
+import NavBar from "./navbar"
+
+function Hero() {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <div>
+                <div>
+                    <h1>totoHacks</h1>
+                    <p>January 2-4, 3028</p>
+                    <button>Apply now</button>
+                </div>
+                <div>
+                   
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/hero/navbar.jsx
@@ -0,0 +1,15 @@
+function NavBar() {
+    return (
+        <div>
+            <ul>
+                <li></li>
+                <li>About</li>
+                <li>Sponsors</li>
+                <li>FAQ</li>
+                <li>Team</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
