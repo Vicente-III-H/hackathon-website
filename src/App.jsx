adde8c428b893a3ab14649bab9d26f3b7456ce94
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2026-01-24 16:43:10 -0500

    Create basic content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import './App.css'
 
+function NavBar() {
+    return (
+        <nav>
+            <div id='logo'>
+
+            </div>
+            <div id='nav-bar'>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Sponsors</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
 function App() {
     return (
         <>
+            <div id='foreground'>
+                <NavBar></NavBar>
+            </div>
+            <div id='content'>
 
+            </div>
+            <div id='background'>
+
+            </div>
         </>
     )
 }
