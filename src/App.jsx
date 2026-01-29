c5a28a782a1921f563d8290b9e19b3e19535fe21
Vicente H <145728917+Vicente-III-H@users.noreply.github.com>
2026-01-28 21:34:14 -0500

    Add base FAQ section structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import './App.css'
 import { aboutImages, largeLogos, smallLogos } from './images'
 
+function SectionHeading({ title, description }) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </div>
+    )
+}
+
 function NavBar() {
     return (
         <nav>
@@ -46,10 +55,7 @@ function StatCard({ number, stat }) {
 function About() {
     return (
         <div id='About' className='flexbox-col-center'>
-            <div>
-                <h1>About</h1>
-                <p>Join a bunch of people doing a bunch of things this Hackathon event! There is a bunch of awesome stuff to do and cool prizes to win! Join one of the most unknown and niche Hackathons ever! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            </div>
+            <SectionHeading title={'About'} description={'Join a bunch of people doing a bunch of things this Hackathon event! There is a bunch of awesome stuff to do and cool prizes to win! Join one of the most unknown and niche Hackathons ever! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}></SectionHeading>
             <div>
                 {aboutImages.map((image) => <img key={image} src={image} width={360} height={240}></img>)}
             </div>
@@ -63,13 +69,32 @@ function About() {
 function Sponsors() {
     return (
         <div id='Sponsors'>
-            <div>
-                <h1>Sponsors</h1>
-                <p>{"Become a sponsors! This is also a placeholder paragraph (well more like text...)"}</p>
-            </div>
+            <SectionHeading title={'Sponsors'} description={'Become a sponsors! This is also a placeholder paragraph (well more like text...)'}></SectionHeading>
             <div id='sponsor-list'>
                 <div id='large-logos'>{largeLogos.map((logo) => <img key={logo} src={logo} width={500} height={100}></img>)}</div>
                 <div id='small-logos'>{smallLogos.map((logo) => <img key={logo} src={logo} width={50} height={50}></img>)}</div>
+            </div>
+        </div>
+    )
+}
+
+function Accordion({ title, description }) {
+    return (
+        <div>
+            <button>{title}</button>
+            <div>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+function FAQ() {
+    return (
+        <div>
+            <SectionHeading title={'FAQ'} description={'Frequently asked questions!'}></SectionHeading>
+            <div>
+                <Accordion title={'Is this a question?'} description={'Yes.'}></Accordion>
             </div>
         </div>
     )
@@ -81,6 +106,7 @@ function App() {
             <Hero></Hero>
             <About></About>
             <Sponsors></Sponsors>
+            <FAQ></FAQ>
         </>
     )
 }
